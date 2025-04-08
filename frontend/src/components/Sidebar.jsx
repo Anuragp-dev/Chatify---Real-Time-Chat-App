357bfc60b9a4baf801788e32bd6f4d7da13a9f36
anuragRexav <107052990+Anuragp-dev@users.noreply.github.com>
2025-04-08 12:12:36 +0530

    changes

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Sidebar = () => {
+    const {getUser, users, selectedUser,setSelectedUser}
+  return (
+    <div>
+      sidebar
+    </div>
+  )
+}
+
+export default Sidebar
